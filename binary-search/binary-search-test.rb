4379f7e68df76bbbd426b3ec401755944245cd51
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-10-20 11:05:08 -0700

    Add find first and last occurances and tests

--- binary-search/binary-search-test.rb
+++ binary-search/binary-search-test.rb
@@ -8,42 +8,58 @@ require "./binary-search.rb"
 require 'basic_test'
 include BasicTest
 
-test "BinarySearch.search, with element present",
-  expected = 0,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 1)
+# test "BinarySearch.search, with element present",
+#   expected = 0,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 1)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 1,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 2,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 3,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 4,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 5,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 6)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 6,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 7)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 7,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 8)
+#
+# test "BinarySearch.search, with element present",
+#   expected = 8,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
+#
+# test "BinarySearch.search, with element not present",
+#   expected = nil,
+#   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 21)
 
-test "BinarySearch.search, with element present",
-  expected = 1,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)
-
-test "BinarySearch.search, with element present",
+test "BinarySearch.search_for, with element present",
   expected = 2,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
+  actual   = BinarySearch.search_for([2, 4, 10, 10, 10, 18], 10, :first)
 
-test "BinarySearch.search, with element present",
-  expected = 3,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4)
-
-test "BinarySearch.search, with element present",
-  expected = 4,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)
-
-test "BinarySearch.search, with element present",
-  expected = 5,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 6)
-
-test "BinarySearch.search, with element present",
-  expected = 6,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 7)
-
-test "BinarySearch.search, with element present",
-  expected = 7,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 8)
-
-test "BinarySearch.search, with element present",
-  expected = 8,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
-
-test "BinarySearch.search, with element not present",
+test "BinarySearch.search_for, with element not present",
   expected = nil,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 21)
+  actual   = BinarySearch.search_for([2, 4, 10, 10, 10, 18], 21, :first)
+
+test "BinarySearch.search_for, with element present",
+  expected = 4,
+  actual   = BinarySearch.search_for([2, 4, 10, 10, 10, 18], 10, :last)
+
+test "BinarySearch.search_for, with element not present",
+  expected = nil,
+  actual   = BinarySearch.search_for([2, 4, 10, 10, 10, 18], 21, :last)
--- binary-search/binary-search.rb
+++ binary-search/binary-search.rb
@@ -18,6 +18,25 @@ module BinarySearch
     end
   end
 
+  def search_for(array, key, which = :first, upper: array.length - 1, lower: 0, result: nil)
+    mid = (upper + lower) / 2
+
+    if lower >= upper
+      result
+    elsif array[mid] == key
+      result = mid
+      upper = mid - 1 if which == :first
+      lower = mid + 1 if which == :last
+      search_for(array, key, upper: upper, lower: lower, result: result)
+    elsif array[mid] > key
+      upper = mid - 1
+      search_for(array, key, which, upper: upper, lower: lower, result: result)
+    else
+      lower = mid + 1
+      search_for(array, key, which, upper: upper, lower: lower, result: result)
+    end
+  end
+
   def profile
     page = Wikipedia.find( 'Binary Search' )
     puts page.summary.split("\n").map { |a| a.strip }.join(" ").gsub(/{.+}/, "")
