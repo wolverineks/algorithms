1599819c04c3264955d529d691d17c7bba916af4
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-10-20 13:33:52 -0700

    Change Binary search to iterative. Add count to binary search, and tests

--- binary-search/binary-search-test.rb
+++ binary-search/binary-search-test.rb
@@ -13,32 +13,8 @@ test "BinarySearch.search, with element present",
   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 1)
 
 test "BinarySearch.search, with element present",
-  expected = 1,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)
-
-test "BinarySearch.search, with element present",
-  expected = 2,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
-
-test "BinarySearch.search, with element present",
   expected = 3,
   actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 4)
-
-test "BinarySearch.search, with element present",
-  expected = 4,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)
-
-test "BinarySearch.search, with element present",
-  expected = 5,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 6)
-
-test "BinarySearch.search, with element present",
-  expected = 6,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 7)
-
-test "BinarySearch.search, with element present",
-  expected = 7,
-  actual   = BinarySearch.search([1, 2, 3, 4, 5, 6, 7, 8, 9], 8)
 
 test "BinarySearch.search, with element present",
   expected = 8,
@@ -56,6 +32,14 @@ test "BinarySearch.search_for, with element not present",
   expected = nil,
   actual   = BinarySearch.search_for([2, 4, 10, 10, 10, 18], 21, :first)
 
+test "BinarySearch.search_for, with element present, short array",
+  expected = 0,
+  actual   = BinarySearch.search_for([10], 10, :first)
+
+test "BinarySearch.search_for, with element not present, short array",
+  expected = nil,
+  actual   = BinarySearch.search_for([10], 21, :first)
+
 test "BinarySearch.search_for, with element present",
   expected = 4,
   actual   = BinarySearch.search_for([2, 4, 10, 10, 10, 18], 10, :last)
@@ -63,3 +47,19 @@ test "BinarySearch.search_for, with element present",
 test "BinarySearch.search_for, with element not present",
   expected = nil,
   actual   = BinarySearch.search_for([2, 4, 10, 10, 10, 18], 21, :last)
+
+test "BinarySearch.search_for, with element present, short array",
+  expected = 0,
+  actual   = BinarySearch.search_for([10], 10, :last)
+
+test "BinarySearch.search_for, with element not present, short array",
+  expected = nil,
+  actual   = BinarySearch.search_for([10], 21, :last)
+
+test "BinarySearch.count, with element present",
+  expected  = 5,
+  actual    = BinarySearch.count([2, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 18], 10)
+
+test "BinarySearch.count, with element not present",
+  expected  = 0,
+  actual    = BinarySearch.count([2, 4, 5, 6, 7, 8, 9, 10, 10, 10, 18], 11)
--- binary-search/binary-search.rb
+++ binary-search/binary-search.rb
@@ -2,39 +2,56 @@ module BinarySearch
   extend self
   require 'wikipedia'
 
-  def search(array, key, upper = array.length - 1, lower = 0)
-    mid = (upper + lower) / 2
+  def search(array, key)
+    lower = 0
+    upper = array.length - 1
+    result = nil
 
-    if lower > upper
-      nil
-    elsif array[mid] == key
-      mid
-    elsif key > array[mid]
-      lower = mid + 1
-      search(array, key, upper, lower)
-    else
-      upper = mid - 1
-      search(array, key, upper, lower)
+    while lower <= upper
+      mid = (upper + lower) / 2
+
+      if array[mid] == key
+        return mid
+      elsif array[mid] > key
+        upper = mid - 1
+      else
+        lower = mid + 1
+      end
     end
   end
 
-  def search_for(array, key, which = :first, upper: array.length - 1, lower: 0, result: nil)
-    mid = (upper + lower) / 2
+  def search_for(array, key, which = :first)
+    lower = 0
+    upper = array.length - 1
+    result = nil
 
-    if lower >= upper
-      result
-    elsif array[mid] == key
-      result = mid
-      upper = mid - 1 if which == :first
-      lower = mid + 1 if which == :last
-      search_for(array, key, upper: upper, lower: lower, result: result)
-    elsif array[mid] > key
-      upper = mid - 1
-      search_for(array, key, which, upper: upper, lower: lower, result: result)
-    else
-      lower = mid + 1
-      search_for(array, key, which, upper: upper, lower: lower, result: result)
+    while lower <= upper
+      mid = (upper + lower) / 2
+
+      if array[mid] == key
+        result = mid
+        upper = mid - 1 if which == :first
+        lower = mid + 1 if which == :last
+      elsif array[mid] > key
+        upper = mid - 1
+      else
+        lower = mid + 1
+      end
     end
+
+    result
+  end
+
+  def count(array, key)
+    lower = 0
+    upper = array.length - 1
+    result = 0
+
+    first = search_for(array, key, :first)
+    return result unless first
+    last  = search_for(array, key, :last)
+
+    result = last - first + 1
   end
 
   def profile
