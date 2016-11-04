10b42716bd2bbbae1a8bfaf14b46064af7da6ffd
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-11-04 13:31:52 -0700

    Add ruby implementation of factorial and tests

--- factorial/ruby/factorial-test.rb
+++ factorial/ruby/factorial-test.rb
@@ -10,22 +10,42 @@ test "Factorial.compute, with no options",
   expected = 1,
   actual   = Factorial.compute(1)
 
-test "Factorial.compute",
+test "Factorial.compute, recursive",
   expected = 2,
-  actual   = Factorial.compute(2)
+  actual   = Factorial.compute(2, :recursive)
 
-test "Factorial.compute",
+test "Factorial.compute, recursive",
   expected = 6,
-  actual   = Factorial.compute(3)
+  actual   = Factorial.compute(3, :recursive)
 
-test "Factorial.compute",
+test "Factorial.compute, recursive",
   expected = 24,
-  actual   = Factorial.compute(4)
+  actual   = Factorial.compute(4, :recursive)
 
-test "Factorial.compute",
+test "Factorial.compute, recursive",
   expected = 120,
-  actual   = Factorial.compute(5)
+  actual   = Factorial.compute(5, :recursive)
 
-test "Factorial.compute",
+test "Factorial.compute, recursive",
   expected = 720,
-  actual   = Factorial.compute(6)
+  actual   = Factorial.compute(6, :recursive)
+
+test "Factorial.compute, iterative",
+  expected = 2,
+  actual   = Factorial.compute(2, :iterative)
+
+test "Factorial.compute, iterative",
+  expected = 6,
+  actual   = Factorial.compute(3, :iterative)
+
+test "Factorial.compute, iterative",
+  expected = 24,
+  actual   = Factorial.compute(4, :iterative)
+
+test "Factorial.compute, iterative",
+  expected = 120,
+  actual   = Factorial.compute(5, :iterative)
+
+test "Factorial.compute, iterative",
+  expected = 720,
+  actual   = Factorial.compute(6, :iterative)
--- factorial/ruby/factorial.rb
+++ factorial/ruby/factorial.rb
@@ -2,9 +2,26 @@ module Factorial
   extend self
   require 'wikipedia'
 
-  def compute(number)
+  def compute(number, method = :recursive)
+    if method == :iterative
+      computeIterative(number)
+    else
+      computeRecursive(number, :recursive)
+    end
+  end
+
+  def computeRecursive(number, method)
     return 1 if number <= 1
     number * compute(number - 1)
+  end
+
+  def computeIterative(number)
+    result = 1
+    while number > 0
+      result *= number
+      number -= 1
+    end
+    result
   end
 
   def profile
